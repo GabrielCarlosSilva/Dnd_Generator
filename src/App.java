53e9e4f74586e8ec6787fa6fac8b6f9315109b82
GabrielCarlosSilva <gabrielcsilva413@hotmail.com>
2022-07-24 12:47:48 -0300

    1.0.0
    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+    
+        Ficha_Personagem p1 = new Ficha_Personagem(12, 12, 12, 14, 14, 13, "Rogue", "Human", "Carlos");
+        p1.Status();
+        
+    
+    }
+    }
